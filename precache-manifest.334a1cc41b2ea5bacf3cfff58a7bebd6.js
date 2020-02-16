2a5f81f066726929a82bf9c8723922c4de2509ba
Elior <ezur6294@gmail.com>
2020-02-16 19:34:24 +0200

    Initial dist subtree commit

---
+++ precache-manifest.334a1cc41b2ea5bacf3cfff58a7bebd6.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "441153d3ef139d763001",
+    "url": "css/app.f071cbdf.css"
+  },
+  {
+    "revision": "9019ab71e2d2d6cc3c9f4140cb27a618",
+    "url": "fonts/Gotham-Book.otf"
+  },
+  {
+    "revision": "0ff4bccb0c544c4b15f5cc47c8bdee5f",
+    "url": "fonts/Gotham-Light.0ff4bccb.otf"
+  },
+  {
+    "revision": "0ff4bccb0c544c4b15f5cc47c8bdee5f",
+    "url": "fonts/Gotham-Light.otf"
+  },
+  {
+    "revision": "27a90e8757eda1edc9872eebec18ba64",
+    "url": "fonts/Gotham-Medium.27a90e87.otf"
+  },
+  {
+    "revision": "27a90e8757eda1edc9872eebec18ba64",
+    "url": "fonts/Gotham-Medium.otf"
+  },
+  {
+    "revision": "7996873c8886169593b38affe6a7a362",
+    "url": "fonts/Gotham-Thin.otf"
+  },
+  {
+    "revision": "f6e21da345a0fc06039dc9cb427e9a42",
+    "url": "fonts/Gotham-Ultra.f6e21da3.otf"
+  },
+  {
+    "revision": "f6e21da345a0fc06039dc9cb427e9a42",
+    "url": "fonts/Gotham-Ultra.otf"
+  },
+  {
+    "revision": "34725f690536489949d52f41e22bd206",
+    "url": "fonts/GothamNarrow-Black.otf"
+  },
+  {
+    "revision": "523cf5c20642f10a901a32d2c320f599",
+    "url": "fonts/GothamNarrow-Book.otf"
+  },
+  {
+    "revision": "4921472b62922c1196fd65185a6318d4",
+    "url": "fonts/GothamNarrow-Thin.otf"
+  },
+  {
+    "revision": "e7fd253c5f344cf7c22035b2618e9b89",
+    "url": "fonts/GothamNarrow-Ultra.otf"
+  },
+  {
+    "revision": "75faca850ede644c0315dd0211a51289",
+    "url": "img/beats cream.75faca85.png"
+  },
+  {
+    "revision": "75faca850ede644c0315dd0211a51289",
+    "url": "img/beats cream.png"
+  },
+  {
+    "revision": "29e40e8a48054796fcf14aa591646926",
+    "url": "img/bitmap man.29e40e8a.png"
+  },
+  {
+    "revision": "29e40e8a48054796fcf14aa591646926",
+    "url": "img/bitmap man.png"
+  },
+  {
+    "revision": "ab934bd286f2ee74e2e0246bdf75fdfc",
+    "url": "img/bitmap.ab934bd2.png"
+  },
+  {
+    "revision": "ab934bd286f2ee74e2e0246bdf75fdfc",
+    "url": "img/bitmap.png"
+  },
+  {
+    "revision": "558b424af6ab23f19be0065410c2b7ba",
+    "url": "img/logo copy.png"
+  },
+  {
+    "revision": "840d1ef6f4b9a87b32063c8167a2bc07",
+    "url": "img/logo.840d1ef6.png"
+  },
+  {
+    "revision": "840d1ef6f4b9a87b32063c8167a2bc07",
+    "url": "img/logo.png"
+  },
+  {
+    "revision": "37c6e32bdfd1a36fca1f6a93cf83fbf7",
+    "url": "img/‎gradient-bg⁩.37c6e32b.png"
+  },
+  {
+    "revision": "37c6e32bdfd1a36fca1f6a93cf83fbf7",
+    "url": "img/‎gradient-bg⁩.png"
+  },
+  {
+    "revision": "e88384aee8997ea2d35e9561f2679d8c",
+    "url": "index.html"
+  },
+  {
+    "revision": "441153d3ef139d763001",
+    "url": "js/app.da5bc074.js"
+  },
+  {
+    "revision": "0ca846a06deaabf92fe4",
+    "url": "js/chunk-vendors.4e4bfd81.js"
+  },
+  {
+    "revision": "6cd746ffd82ddf2903519139ca93d863",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
